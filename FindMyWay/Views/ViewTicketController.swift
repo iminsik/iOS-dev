1f1e80cdf57855d7058609328644d783e3816d53
Insik Cho <iminsik@gmail.com>
2018-12-05 16:13:05 -0800

    added initial routine to checking FlightInfoEntity db

--- FindMyWay/Views/ViewTicketController.swift
+++ FindMyWay/Views/ViewTicketController.swift
@@ -16,8 +16,8 @@ import PromiseKit
 class ViewTicketController: UIViewController {
     @IBOutlet weak var TextViewPNR: UITextView!
     // Q: How can we secure username and password?
-    let username: String = ""
-    let password: String = ""
+    let username: String = "kimate"
+    let password: String = "6ea9e24e929403785d2f2bd99684a76fda3aed14"
     static var scheduler : Timer?
 
     fileprivate func ShowError(err: Error) -> Void {
@@ -36,6 +36,8 @@ class ViewTicketController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.AddRandomFlightToEntity()
+        
         if ViewTicketController.scheduler == nil {
             ViewTicketController.scheduler = Timer.scheduledTimer(timeInterval: 20.0, target: self, selector: #selector(self.AddRandomFlightToEntity), userInfo: nil, repeats: true)
         }
